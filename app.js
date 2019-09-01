de1e37ee5387f30fb347b5c4eece1ee85b0e722d
Oscar Ko <OscarKoEmail@gmail.com>
2019-09-01 15:44:05 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// REQUIREMENTS =====================================
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// ROUTES ============================================
+
+app.get("/", function(req, res){
+  res.send("Hello");
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
