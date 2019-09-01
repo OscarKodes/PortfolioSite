1717c9ac0eaf9341c0cbe695cbac125e0af8cfc1
Oscar Ko <OscarKoEmail@gmail.com>
2019-09-01 16:40:18 -0400

    Create projects, skill-set, resume, and contact routes and pages

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ app.get("/", function(req, res){
 
 app.get("/front", function(req, res){
   res.render("front");
-})
+});
+
+app.get("/projects", function(req, res){
+  res.render("projects");
+});
+
+app.get("/skill-set", function(req, res){
+  res.render("skill-set");
+});
+
+app.get("/resume", function(req, res){
+  res.render("resume");
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
